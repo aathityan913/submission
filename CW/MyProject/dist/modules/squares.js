010fa473452172531dd4a481068dfc68a9df5db4
Aathityan Mariraj <aathityan.m@cspl.com>
2025-11-06 22:59:41 +0530

    build: configure Babel and Webpack for production-ready ES6 build and generate bundle.js in dist

--- CW/MyProject/dist/modules/squares.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.squares = squares;
-function* squares(n) {
-    for (let i = 1; i <= n; i++) {
-        yield Math.pow(i, 2);
-    }
-}
