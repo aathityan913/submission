871570ec0a33273cbbaed3ab434bed126a2e3e00
Aathityan Mariraj <aathityan.m@cspl.com>
2025-11-06 16:48:30 +0530

    Some changes

--- BankCustomerApi/BankCustomerApi/obj/Debug/net9.0/BankCustomerApi.AssemblyInfo.cs
+++ BankCustomerApi/BankCustomerApi/obj/Debug/net9.0/BankCustomerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankCustomerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09910ed6cc0dbd406ea1ac6e5f9b17b6bdc431d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8e6de5d7f47af6676484154c7eace3583a225a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankCustomerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankCustomerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
